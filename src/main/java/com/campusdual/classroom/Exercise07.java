56628dd6073b293b6ba60311cd05bf8e741f3e8b
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-15 13:25:49 +0200

    Ejercicio 7 finalizado

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,9 +1,26 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
+        positionInAList(4);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el valor e N: ");
+        int n = scanner.nextInt();
+        sumFirstNaturalNumbers(n);
+        scanner.close();
+
+        System.out.println("Introduce el valor e N: ");
+        int num = scanner.nextInt();
+        sumFirstNaturalNumbers(num);
+        scanner.close();
+        showFirstNaturalNumbers(num);
 
     }
 
@@ -15,17 +32,60 @@ public class Exercise07 {
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
 
+        List<Integer> NumberList = new ArrayList<>();
+        NumberList.add(1);
+        NumberList.add(2);
+        NumberList.add(3);
+        NumberList.add(4);
+        NumberList.add(5);
+        NumberList.add(6);
+        NumberList.add(7);
+        NumberList.add(8);
+        NumberList.add(9);
+        NumberList.add(10);
+
+        int result = -1;
+
+        for (int i=0; i <NumberList.size(); i++){
+            if (NumberList.get(i) == num){
+                result = 1;
+                System.out.println("El elemento X se encuentra en la posición: Y");
+            }
+            if (result == -1){
+                System.out.println("El elemento X no se encuentra en la lista.");
+
+            }
+
+
+        }
+
+
     }
 
     //TODO ↓
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
+    int sum = 0;
+        for (int i = 1; i <=num; i++){
+            sum +=i;
+        }
+
+        System.out.println(sum);
+
 
     }
 
     //TODO ↓
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
+
+        int sum = 0;
+        for (int i = 0; i <=num; i++){
+            sum =sum+1;
+            System.out.println(sum);
+        }
+
+
 
     }
 
